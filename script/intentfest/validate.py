94a0af18003434f304a38e9fb315ab4b12f03d61
Paulus Schoutsen <balloob@gmail.com>
2022-12-15 21:41:59 -0500

    Validate the YAML files against schema (#9)
    * Validate the YAML files against schema
    
    * Fix imports
    
    * Cleanup
    
    * Fix typing
    
    * Single dict key for lists
    
    * Add type to range example
    
    * Address comments
    
    * remove validation checks from tests
    
    * Validate tests/common
    
    * Remove unused vol.All
    
    * Add markers around all dict keys in schemas

--- script/intentfest/validate.py
+++ script/intentfest/validate.py
@@ -2,11 +2,145 @@
 from __future__ import annotations
 
 import argparse
+from typing import Any
 
+import voluptuous as vol
 import yaml
+from voluptuous.humanize import validate_with_humanized_errors
 
 from .const import INTENTS_FILE, LANGUAGES, SENTENCE_DIR, TESTS_DIR
 from .util import get_base_arg_parser, require_sentence_domain_slot
+
+
+def match_anything(value):
+    """Validator that matches everything"""
+    return value
+
+
+def single_key_dict_validator(schemas: dict[str, Any]) -> vol.Schema:
+    """Create a validator for a single key dict."""
+
+    def validate(value):
+        if not isinstance(value, dict):
+            raise vol.Invalid("Expected a dict")
+
+        if len(value) != 1:
+            raise vol.Invalid("Expected a single key dict")
+
+        key = next(iter(value))
+
+        if key not in schemas:
+            raise vol.Invalid(f"Expected a key in {', '.join(schemas)}")
+
+        if not isinstance(schemas[key], vol.Schema):
+            schemas[key] = vol.Schema(schemas[key])
+
+        return schemas[key](value[key])
+
+    return validate
+
+
+INTENTS_SCHEMA = vol.Schema(
+    {
+        str: {
+            vol.Required("description"): str,
+            vol.Optional("slots"): {
+                str: {
+                    vol.Required("description"): str,
+                    vol.Optional("required"): bool,
+                }
+            },
+            vol.Optional("slot_combinations"): {str: [str]},
+        }
+    }
+)
+
+INTENT_ERRORS = {
+    "no_intent",
+    "no_area",
+    "no_domain",
+    "no_device_class",
+    "no_entity",
+    "handle_error",
+}
+
+SENTENCE_SCHEMA = vol.Schema(
+    {
+        vol.Required("language"): str,
+        vol.Optional("intents"): {
+            str: {
+                vol.Required("data"): [
+                    {
+                        vol.Required("sentences"): [str],
+                        vol.Optional("slots"): {str: match_anything},
+                    }
+                ]
+            }
+        },
+        vol.Optional("lists"): {
+            str: single_key_dict_validator(
+                {
+                    "values": [
+                        vol.Any(
+                            str,
+                            {"in": str, "out": match_anything},
+                        )
+                    ],
+                    "range": {
+                        vol.Required("type", default="number"): str,
+                        vol.Required("from"): int,
+                        vol.Required("to"): int,
+                        vol.Optional("step", default=1): int,
+                    },
+                }
+            )
+        },
+        vol.Optional("expansion_rules"): {str: str},
+        vol.Optional("skip_words"): [str],
+        vol.Optional("responses"): {
+            vol.Optional("errors"): {
+                vol.In(INTENT_ERRORS): str,
+            }
+        },
+    }
+)
+
+TESTS_SCHEMA = vol.Schema(
+    {
+        vol.Required("language"): str,
+        vol.Required("tests"): [
+            {
+                vol.Required("sentences"): [str],
+                vol.Required("intent"): {
+                    vol.Required("name"): str,
+                    vol.Optional("slots"): {
+                        str: {vol.Required("value"): match_anything}
+                    },
+                },
+            }
+        ],
+    }
+)
+
+TESTS_COMMON = vol.Schema(
+    {
+        vol.Required("language"): str,
+        vol.Optional("areas"): [
+            {
+                vol.Required("name"): str,
+                vol.Required("id"): str,
+            }
+        ],
+        vol.Optional("entities"): [
+            {
+                vol.Required("name"): str,
+                vol.Required("id"): str,
+                vol.Required("area"): str,
+                vol.Required("domain"): str,
+            }
+        ],
+    }
+)
 
 
 def get_arguments() -> argparse.Namespace:
@@ -28,6 +162,12 @@ def run() -> int:
 
     intent_schemas = yaml.safe_load(INTENTS_FILE.read_text())
 
+    try:
+        validate_with_humanized_errors(intent_schemas, INTENTS_SCHEMA)
+    except vol.Error as err:
+        print(f"File intents.yaml has invalid format: {err}")
+        return 1
+
     errors: dict[str, list[str]] = {}
 
     for language in languages:
@@ -38,11 +178,14 @@ def run() -> int:
             errors.pop(language)
 
     if errors:
-        print("Validation failed:")
+        print("Validation failed")
+        print()
+
         for language, language_errors in errors.items():
             print(f"Language: {language}")
             for error in language_errors:
                 print(f" - {error}")
+            print()
         return 1
 
     print("All good!")
@@ -58,12 +201,22 @@ def validate_language(intent_schemas, language, errors):
     for language_file in language_dir.iterdir():
         language_files.add(language_file.name)
 
-        if language_file.name == "_common.yaml":
-            info = yaml.safe_load(language_file.read_text())
-            if info["language"] != language:
-                errors[language].append(
-                    f"File {language_file.name} references incorrect language {info['language']}"
-                )
+        content = yaml.safe_load(language_file.read_text())
+
+        try:
+            validate_with_humanized_errors(content, SENTENCE_SCHEMA)
+        except vol.Error as err:
+            errors[language].append(
+                f"File {language_file.name} has invalid format: {err}"
+            )
+            continue
+
+        if content["language"] != language:
+            errors[language].append(
+                f"File {language_file.name} references incorrect language {content['language']}"
+            )
+
+        if language_file.name.startswith("_"):
             continue
 
         domain, intent = language_file.stem.split("_")
@@ -74,13 +227,12 @@ def validate_language(intent_schemas, language, errors):
             )
             continue
 
-        sentences = yaml.safe_load(language_file.read_text())
-        if sentences["language"] != language:
+        if content["language"] != language:
             errors[language].append(
-                f"File {language_file.name} references incorrect language {sentences['language']}"
+                f"File {language_file.name} references incorrect language {content['language']}"
             )
 
-        for intent_name, intent_info in sentences["intents"].items():
+        for intent_name, intent_info in content["intents"].items():
             if intent != intent_name:
                 errors[language].append(
                     f"File {language_file.name} references incorrect intent {intent_name}. Only {intent} allowed"
@@ -107,11 +259,22 @@ def validate_language(intent_schemas, language, errors):
 
         language_files.discard(test_file.name)
 
-        info = yaml.safe_load(test_file.read_text())
+        content = yaml.safe_load(test_file.read_text())
 
-        if info["language"] != language:
+        if test_file.name == "_common.yaml":
+            schema = TESTS_COMMON
+        else:
+            schema = TESTS_SCHEMA
+
+        try:
+            validate_with_humanized_errors(content, schema)
+        except vol.Error as err:
+            errors[language].append(f"File {test_file.name} has invalid format: {err}")
+            continue
+
+        if content["language"] != language:
             errors[language].append(
-                f"Test {test_file.name} references incorrect language {info['language']}"
+                f"Test {test_file.name} references incorrect language {content['language']}"
             )
 
         if test_file.name == "_common.yaml":
@@ -119,7 +282,7 @@ def validate_language(intent_schemas, language, errors):
 
         domain, intent = test_file.stem.split("_")
 
-        tested_intents = set(i["intent"]["name"] for i in info["tests"])
+        tested_intents = set(i["intent"]["name"] for i in content["tests"])
 
         if intent not in tested_intents:
             errors[language].append(
