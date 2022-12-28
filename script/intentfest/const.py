55296e99481890f72898c5348f320b69333c2b85
Paulus Schoutsen <balloob@gmail.com>
2022-12-28 21:23:28 +0000

    Make sure languages are always sorted

--- script/intentfest/const.py
+++ script/intentfest/const.py
@@ -7,4 +7,4 @@ SENTENCE_DIR = ROOT / "sentences"
 RESPONSE_DIR = ROOT / "responses"
 TESTS_DIR = ROOT / "tests"
 INTENTS_FILE = ROOT / "intents.yaml"
-LANGUAGES = [p.name for p in SENTENCE_DIR.iterdir() if p.is_dir()]
+LANGUAGES = sorted(p.name for p in SENTENCE_DIR.iterdir() if p.is_dir())
