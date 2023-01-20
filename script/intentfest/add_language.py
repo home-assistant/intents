dfc65c907c1fe541f2bf094d6ed65eac778bc9bb
Michael Hansen <mike@rhasspy.org>
2023-01-20 11:23:12 -0600

    Add response keys during matching (#691)
    * Validate response keys in intents
    
    * Add English responses for all intents
    
    * Copy English responses in add_language
    
    * Migrate "success" response keys to "default"
    
    * Don't check "success" in merged_output
    
    * Working on English responses
    
    * Extend en color set sentences
    
    * First pass at jinja2 response template validation
    
    * Add requires context for "set <name> to 50%"
    
    * Validate response template variables against intents.yaml slots
    
    * Clean up fake slots
    
    * Skip missing response key validation if no sentences are defined for an intent

--- script/intentfest/add_language.py
+++ script/intentfest/add_language.py
@@ -44,7 +44,7 @@ def run() -> int:
         yaml.dump, sort_keys=False, allow_unicode=True, Dumper=YamlDumper
     )
 
-    intent_schemas = yaml.safe_load(INTENTS_FILE.read_text())
+    intent_schemas = yaml.safe_load(INTENTS_FILE.read_text(encoding="utf-8"))
 
     # Create language directory
     sentence_dir = SENTENCE_DIR / language
@@ -98,12 +98,12 @@ def run() -> int:
                 "language": language,
                 "responses": {
                     "errors": {
-                        "no_intent": "TODO Sorry, I couldn't understand that",
-                        "no_area": "TODO No area named {{ area }}",
-                        "no_domain": "TODO {{ area }} does not contain a {{ domain }}",
-                        "no_device_class": "TODO {{ area }} does not contain a {{ device_class }}",
-                        "no_entity": "TODO No device or entity named {{ entity }}",
-                        "handle_error": "TODO An unexpected error occurred while handling the intent",
+                        "no_intent": "Sorry, I couldn't understand that",
+                        "no_area": "No area named {{ area }}",
+                        "no_domain": "{{ area }} does not contain a {{ domain }}",
+                        "no_device_class": "{{ area }} does not contain a {{ device_class }}",
+                        "no_entity": "No device or entity named {{ entity }}",
+                        "handle_error": "An unexpected error occurred while handling the intent",
                     },
                 },
                 "lists": {},
@@ -175,28 +175,25 @@ def run() -> int:
         )
     )
 
+    # Create response files based off English
     english_responses = RESPONSE_DIR / "en"
 
     for english_filename in english_responses.iterdir():
         intent = english_filename.stem
+        with open(english_filename, "r", encoding="utf-8") as english_file:
+            responses = yaml.safe_load(english_file)["responses"]
 
         (response_dir / english_filename.name).write_text(
             yaml_dump(
                 {
                     "language": language,
-                    "responses": {
-                        "intents": {
-                            intent: {
-                                "success": [],
-                            },
-                        },
-                    },
+                    "responses": responses,
                 },
             )
         )
 
     # Update languages.yaml
-    languages = yaml.safe_load(LANGUAGES_FILE.read_text())
+    languages = yaml.safe_load(LANGUAGES_FILE.read_text(encoding="utf-8"))
     languages[language] = {
         "nativeName": args.native_name,
     }
