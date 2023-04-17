9c777ac6c271e0b405f0f4cec81aba55970bd85a
Michael Hansen <mike@rhasspy.org>
2023-04-17 11:08:21 -0500

    Generate domain/language file during packaging

--- package/home_assistant_intents/__init__.py
+++ package/home_assistant_intents/__init__.py
@@ -1,11 +1,12 @@
+"""API for home_assistant_intents package."""
+import importlib.resources
 import json
 import os
 import typing
-from collections import defaultdict
 from pathlib import Path
-from typing import Any, Callable, Dict, IO, List, Optional
+from typing import IO, Any, Callable, Dict, List, Optional
 
-import importlib.resources
+from .domains import DOMAINS_AND_LANGUAGES
 
 _PACKAGE = "home_assistant_intents"
 _DIR = Path(typing.cast(os.PathLike, importlib.resources.files(_PACKAGE)))
@@ -28,15 +29,4 @@ def get_intents(
 
 def get_domains_and_languages() -> Dict[str, List[str]]:
     """Return a dict of available domains and languages."""
-    domains_and_languages: Dict[str, List[str]] = defaultdict(list)
-
-    # data/<domain>/<language>.json
-    for domain_dir in sorted(_DATA_DIR.iterdir()):
-        if not domain_dir.is_dir():
-            continue
-
-        domains_and_languages[domain_dir.name] = sorted(
-            (language_file.stem for language_file in domain_dir.glob("*.json"))
-        )
-
-    return domains_and_languages
+    return DOMAINS_AND_LANGUAGES
