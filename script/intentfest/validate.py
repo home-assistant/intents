9095c0a9e16479fedfa64c099ed937cbea8cf9d9
Michael Hansen <mike@rhasspy.org>
2024-03-27 12:06:33 -0500

    Allow metadata in responses

--- script/intentfest/validate.py
+++ script/intentfest/validate.py
@@ -6,6 +6,7 @@ import argparse
 from collections import Counter, defaultdict
 from pathlib import Path
 from typing import Any
+from unittest.mock import MagicMock
 
 import jinja2
 import regex
@@ -148,6 +149,7 @@ SENTENCE_SCHEMA = vol.Schema(
                         vol.Optional("requires_context"): {str: match_anything},
                         vol.Optional("excludes_context"): {str: match_anything},
                         vol.Optional("response"): str,
+                        vol.Optional("metadata"): {str: match_anything},
                     }
                 ]
             }
@@ -541,6 +543,7 @@ def validate_language(
                                 "slots": slots,
                                 "query": {"matched": [], "unmatched": []},
                                 "state_attr": lambda *args: None,
+                                "metadata": MagicMock(),
                             }
                         )
                     except jinja2.exceptions.TemplateError as err:
