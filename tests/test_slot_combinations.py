c92dbad18da4072e2d6357f299f7c6fbc90b3cc3
Michael Hansen <mike@rhasspy.org>
2025-04-21 16:16:26 -0500

    Working on tests

---
+++ tests/test_slot_combinations.py
@@ -0,0 +1,280 @@
+import sys
+import warnings
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any
+
+import pytest
+import yaml
+from hassil import Intents, SlotList, TextSlotList, merge_dict, recognize_best
+
+from . import (
+    BASE_DIR,
+    INTENTS_FILE,
+    LISTS_DIR,
+    RESPONSES_DIR,
+    RULES_DIR,
+    SENTENCES_DIR,
+    TESTS_DIR,
+)
+
+CONTEXT_AREA_NAME = "__context_area__"
+
+
+@dataclass
+class LanguageResources:
+    language: str
+    """Language code."""
+
+    intents: Intents
+    """Compiled intents for language."""
+
+    responses: dict[str, dict[str, str]]
+    """Response by intent -> key"""
+
+
+@pytest.fixture(name="lang_resources", scope="session")
+def lang_resources_fixture(language: str, intent_schemas: dict[str, Any]):
+    lang_intents_dict: dict[str, Any] = {
+        "language": language,
+        "intents": {},
+        "lists": {},
+        "expansion_rules": {},
+    }
+
+    # Load expansion rules
+    rules_dict: dict[str, Any] = lang_intents_dict["expansion_rules"]
+    for rule_path in (RULES_DIR / language).glob("*.yaml"):
+        with open(rule_path, "r", encoding="utf-8") as rule_file:
+            rules_dict.update(yaml.safe_load(rule_file))
+
+    # Load shared lists
+    lists_dict: dict[str, Any] = lang_intents_dict["lists"]
+    for list_path in LISTS_DIR.glob("*.yaml"):
+        with open(list_path, "r", encoding="utf-8") as list_file:
+            lists_dict.update(yaml.safe_load(list_file))
+
+    # Load language-specific lists
+    for list_path in (LISTS_DIR / language).glob("*.yaml"):
+        with open(list_path, "r", encoding="utf-8") as list_file:
+            list_dict = yaml.safe_load(list_file)
+            assert list_dict.pop("language") == language
+            lists_dict.update(list_dict)
+
+    # TODO: better errors
+    responses: dict[str, dict[str, str]] = {}
+    for intent_name, intent_info in intent_schemas.items():
+        intent_dict = lang_intents_dict["intents"].get(intent_name, {"data": []})
+        intent_data = intent_dict["data"]
+
+        responses_path = RESPONSES_DIR / language / f"{intent_name}.yaml"
+        if responses_path.exists():
+            with open(responses_path, "r", encoding="utf-8") as responses_file:
+                responses_dict = yaml.safe_load(responses_file)
+
+            responses[intent_name] = responses_dict["responses"]["intents"][intent_name]
+        else:
+            responses[intent_name] = {}  # no responses
+
+        for combo_name, combo_info in intent_info["slot_combinations"].items():
+            sentences_path = (
+                SENTENCES_DIR / language / intent_name / f"{combo_name}.yaml"
+            )
+            if not sentences_path.exists():
+                continue
+
+            with open(sentences_path, "r", encoding="utf-8") as sentences_file:
+                combo_dict = yaml.safe_load(sentences_file)
+                assert combo_dict.pop("language") == language, sentences_path
+
+                combo_dict_slots = combo_dict.get("slots", {})
+                combo_dict_metadata = combo_dict.get("metadata", {})
+                combo_dict_requires_context = combo_dict.get("requires_context", {})
+
+                if name_domains := combo_info.get("name_domains"):
+                    combo_dict_requires_context["domain"] = name_domains
+                elif inferred_domain := combo_info.get("inferred_domain"):
+                    combo_dict_slots["domain"] = inferred_domain
+
+                # Add context area slot
+                if combo_info.get("context_area"):
+                    combo_dict_requires_context["area"] = {"slot": True}
+
+                # Attach metadata so we can check the slot combination later
+                combo_dict_metadata["slot_combination"] = combo_name
+                combo_dict_metadata["sentence_templates"] = combo_dict["sentences"]
+
+                if combo_dict_slots:
+                    combo_dict["slots"] = combo_dict_slots
+
+                combo_dict["metadata"] = combo_dict_metadata
+
+                if combo_dict_requires_context:
+                    combo_dict["requires_context"] = combo_dict_requires_context
+
+                intent_data.append(combo_dict)
+
+        lang_intents_dict["intents"][intent_name] = intent_dict
+
+    return LanguageResources(
+        language=language,
+        intents=Intents.from_dict(lang_intents_dict),
+        responses=responses,
+    )
+
+
+def do_test_slot_combination(
+    lang_resources: LanguageResources,
+    intent_name: str,
+    combo_name: str,
+    combo_info: dict[str, Any],
+) -> None:
+    test_file_path = (
+        TESTS_DIR / lang_resources.language / intent_name / f"{combo_name}.yaml"
+    )
+    error_info = (
+        f"language={lang_resources.language}, "
+        f"intent={intent_name}, "
+        f"slot_combination={combo_name}, "
+        f"file={test_file_path.relative_to(BASE_DIR)}"
+    )
+
+    if combo_info.get("importance") == "required":
+        assert test_file_path.exists(), f"Required test file is missing: {error_info}"
+    elif not test_file_path.exists():
+        # warnings.warn(
+        #     UserWarning(
+        #         f"Missing test for language '{lang_resources.language}': {test_file_path}"
+        #     )
+        # )
+        return
+
+    with open(test_file_path, "r", encoding="utf-8") as test_file:
+        test_dict = yaml.safe_load(test_file)
+
+    # Load test fixtures
+    slot_lists = {
+        "name": TextSlotList.from_tuples(
+            [
+                (e["name"], e["name"], {"domain": e["domain"]})
+                for e in test_dict.get("entities", [])
+            ],
+            name="name",
+        ),
+        "area": TextSlotList.from_strings(
+            [a["name"] for a in test_dict.get("areas", [])], name="area"
+        ),
+        "floor": TextSlotList.from_strings(
+            [f["name"] for f in test_dict.get("floors", [])], name="floor"
+        ),
+    }
+
+    # For quick look-up during individual tests
+    entity_domains_by_name: dict[str, set[str]] = defaultdict(set)
+    for test_entity in test_dict.get("entities", []):
+        entity_domains_by_name[test_entity["name"]].add(test_entity["domain"])
+
+    possible_slot_names = set(combo_info["slots"])
+    name_domains = set(combo_info.get("name_domains", []))
+    inferred_domain = combo_info.get("inferred_domain")
+
+    # Retrieved from metadata
+    untested_sentence_templates: set[str] = set()
+
+    # TODO: add validation in script
+    for test_group in test_dict["tests"]:
+        expected_slots = test_group.get("slots", {})
+
+        if inferred_domain:
+            expected_slots["domain"] = inferred_domain
+
+        expected_slot_names = expected_slots.keys()
+        assert expected_slot_names == possible_slot_names
+
+        expected_response = test_group["response"]
+
+        for test_sentence in test_group["sentences"]:
+            result = recognize_best(
+                test_sentence,
+                lang_resources.intents,
+                slot_lists=slot_lists,
+                intent_context={"area": CONTEXT_AREA_NAME},
+                best_slot_name="name",
+            )
+            # TODO: better errors
+            assert (
+                result is not None
+            ), f"Sentence was not recognized: '{test_sentence}', {error_info}"
+            assert result.intent.name == intent_name
+            assert result.intent_metadata is not None
+            assert result.intent_metadata.get("slot_combination") == combo_name
+
+            if not untested_sentence_templates:
+                untested_sentence_templates.update(
+                    result.intent_metadata["sentence_templates"]
+                )
+
+            untested_sentence_templates.discard(result.intent_sentence.text)
+
+            # print(test_sentence)
+            # TODO: render template
+            actual_response = lang_resources.responses.get(intent_name, {}).get(
+                result.response, ""
+            )
+            assert actual_response == expected_response
+
+            actual_slots = {e_name: e.value for e_name, e in result.entities.items()}
+
+            if combo_info.get("context_area"):
+                # Remove context area
+                assert actual_slots.pop("area") == CONTEXT_AREA_NAME
+
+            actual_slot_names = actual_slots.keys()
+
+            if name_domains:
+                actual_name = actual_slots["name"]
+                assert actual_name in entity_domains_by_name
+                assert entity_domains_by_name[actual_name].issubset(name_domains)
+            elif inferred_domain:
+                assert actual_slots.get("domain") == inferred_domain
+
+            assert expected_slot_names == actual_slot_names
+
+            for actual_slot_name in actual_slot_names:
+                actual_slot_value = actual_slots[actual_slot_name]
+                expected_slot_value = expected_slots[actual_slot_name]
+                if isinstance(actual_slot_value, list):
+                    # Multiple values are possible for some slots.
+                    # For example, "open the curtains" may match shades as well.
+                    assert expected_slot_value in actual_slot_value
+                else:
+                    assert expected_slot_value == actual_slot_value
+
+    assert (
+        not untested_sentence_templates
+    ), f"{len(untested_sentence_templates)} untested sentence template(s): {error_info}"
+
+
+def gen_test(intent_name: str, combo_name: str, combo_info: dict[str, Any]) -> None:
+    def test_func(lang_resources) -> None:
+        do_test_slot_combination(
+            lang_resources,
+            intent_name=intent_name,
+            combo_name=combo_name,
+            combo_info=combo_info,
+        )
+
+    test_func.__name__ = f"test_{intent_name}_{combo_name}"
+    setattr(sys.modules[__name__], test_func.__name__, test_func)
+
+
+def gen_tests() -> None:
+    with open(INTENTS_FILE, "r", encoding="utf-8") as schemas_file:
+        intent_schemas = yaml.safe_load(schemas_file)
+
+    for intent_name, intent_info in sorted(intent_schemas.items()):
+        for combo_name, combo_info in sorted(intent_info["slot_combinations"].items()):
+            gen_test(intent_name, combo_name, combo_info)
+
+
+gen_tests()
